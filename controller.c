f1c1f679ea44cc455fb8922213d3b5a77c9cadad
Nick Firmani <nick.firmani@ieee.org>
2014-12-12 10:26:20 +0000

    Debugged. Kinda

--- controller.c
+++ controller.c
@@ -2,56 +2,63 @@
 #include <stdio.h>
 #include <stdint.h>
 
+#ifndef _TC_C
+ 
+#define _TC_C
+
+void cleanup();
+float getThermoCoupleTemp();
+int init();
+int set_to_temp(int temp);
+
 int main() {
     int n = 100;
     int TEMP = 1000;
+
     if (!init()) {
         //TODO Print error message failed on init code
         return 1;
     }
-    
+
+ 
     //Testing loop
     while (n > 0) {
-        printf("Current temperature: %f\n" % getThermoCoupleTemp());
+        printf("Current temperature: %f\n", getThermoCoupleTemp());
         if (set_to_temp(TEMP)) {
             n--;
         }
     }
-    
-    if (!cleanup()) {
-        //todo raise error
-    } else {
-        return 0
-    }
+    cleanup();
+    return 0;
 }
-
+ 
 int init() {
-	if (!bcm2835_init()) {
-		return 1;
-	}
+    if (!bcm2835_init()) {
+        return 1;
+    }
     bcm2835_spi_begin();
-    
+ 
     //BCM2835_SPI_CS0 = Chip Select 0 // 0 = Active (CS low when TX/RX)
     bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, 0);
-    
+ 
     //BCM2835_SPI_CLOCK_DIVIDER_8192  = 8192, // 8192 = 32.768us = 30.51757813kHz
     bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_8192);
-    
+ 
     //BCM2835_SPI_MODE0 = 0,  // CPOL = 0, CPHA = 0, Clock idle low, data is clocked in on rising edge, output data (change) on falling edge
     bcm2835_spi_setDataMode(BCM2835_SPI_MODE0);
 }
-
+ 
 /**
  * @return The thermocouple temperature as a float.
  */
 float getThermoCoupleTemp() {
     uint32_t DATALEN = 4;
-    char tbuf = {0x00, 0x00, 0x00, 0x00};
-    bcm2835_spi_transfern(tbuf, len); //data is now loaded into tbuf
-
+    char tbuf[4] = {0x00, 0x00, 0x00, 0x00};
+    bcm2835_spi_transfern(tbuf, sizeof(tbuf)); //data is now loaded into tbuf
+ 
     uint16_t res = tbuf[0] << 8 | (tbuf[1] & 0xFF);
     float tc_temp = (res >> 2) / 4.00f; //Does not support negative temperatures TODO future release
-  
+ 
     //Error Checking
     if (tbuf[1] & 0x02 || tbuf[3] & 0x08) {
         //programming error dummy (reserved bits high)
@@ -66,13 +73,13 @@ float getThermoCoupleTemp() {
         if (tbuf[3] & 0x01) {
             //print open circuit
         }
-        return -1f;
+        return -1.0f;
     }
     return tc_temp;
 }
-
+ 
 int set_to_temp(int temp) {
-    if (temp < getThermoCoupleTemp() { 
+    if (temp < getThermoCoupleTemp()) {
         bcm2835_gpio_clr(RPI_V2_GPIO_P1_11);
         return 0;
     } else {
@@ -80,7 +87,9 @@ int set_to_temp(int temp) {
         return 1;
     }
 }
-
+ 
 void cleanup() {
-    return bcm2835_spi_end();
+    void bcm2835_spi_end();
 }
+
+#endif
